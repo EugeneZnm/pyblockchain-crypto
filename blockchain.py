b80d59bc59c0f4a46b01dac968c0fbfa9ac80880
EugeneZnm <eugenenzioki@gmail.com>
2018-10-11 16:44:45 +0300

    chaining data, mining new blocks/ adding new coins, bbasic lock hashing

--- blockchain.py
+++ blockchain.py
@@ -4,10 +4,16 @@ blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Returning latest value of current blockchain """
     return blockchain[-1]
 
 
 def add_value(coins, last_transaction=[1]):
+    """ Appending new value of blockchain as well as last value of blockchain to blockchain
+
+    coins =  amount added into blockchain after every transacion
+    last-transaction = added to blockchain after
+    """
     blockchain.append([last_transaction, coins])
 
 
