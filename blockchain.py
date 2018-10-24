d0405638c1fdba5905e2030f00767de01876e800
EugeneZnm <eugenenzioki@gmail.com>
2018-10-24 10:07:56 +0300

    enabling entry pf transaction values, and choosing between output of blockchain and adding new transaction vlaues

--- blockchain.py
+++ blockchain.py
@@ -17,17 +17,53 @@ def add_value(coins, last_transaction=[1]):
     blockchain.append([last_transaction, coins])
 
 
-def get_user_input():
-    return float(input('Input amount: '))
+def get_transaction_value():
+    """
+    function to get transaction value
+    :return:
+    """
+    user_input = float(input('Input amount: '))
+    return user_input
 
+def get_user_choice():
+    """
+    user choosing course of action
+    :return:
+    """
+    user_input = input('your choice')
+    return user_input
 
-tx_amount = get_user_input()
+def print_blockchain_elements():
+    """
+    output blockchain elements
+    :return:
+    """
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), coins=tx_amount)
+# tx_amount = get_user_input()
+# add_value(last_transaction=get_last_blockchain_value(), coins=tx_amount)
+#
+# # get transaction input and add value ot the blockchain
+# tx_amount = get_user_input()
+# add_value(tx_amount, get_last_blockchain_value())
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+while True:
+    print('please choose')
+    print('1: Add new transaction value')
+    print('2: Output the blockchain blocks')
 
-print(blockchain)
+    # getting choice form user
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
+
+
+    print('Done!')
